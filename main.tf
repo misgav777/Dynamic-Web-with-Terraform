f095b84af71b5de17692d81891c2743ce930fe44
misgav777 <misgav777@gmail.com>
2023-09-26 12:36:55 +0300

    create vpc with 6 subnets with 2 az

---
+++ main.tf
@@ -0,0 +1,14 @@
+#configure aws provider
+provider "aws" {
+  region  = "us-east-1"
+  profile = "misgav"
+}
+
+#store the terraform state file in s3
+terraform {
+  backend "s3" {
+    bucket = "misgavterraformstate"
+    key    = "terraform.tfstate.dev"
+    region = "us-east-1"
+  }
+}
