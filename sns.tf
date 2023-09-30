78215eb2a9c9935791f7086a90382bd83c3e04f6
misgav777 <misgav777@gmail.com>
2023-09-30 20:53:12 +0300

    created sns resource file

--- sns.tf
+++ sns.tf
@@ -6,6 +6,6 @@ resource "aws_sns_topic" "user_updates" {
 # create an sns topic subscription
 resource "aws_sns_topic_subscription" "notification_topic" {
   topic_arn = aws_sns_topic.user_updates.arn
-  protocol  = "email"
   endpoint  = var.operator_email
+  protocol  = "email"
 }
